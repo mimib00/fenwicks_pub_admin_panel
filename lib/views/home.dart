8ee0fafbe08d4bfcfa734345a8a9ebedebd16228
Melkia Mohamed <83797689+mimib00@users.noreply.github.com>
2022-05-21 12:35:19 +0100

    fix #1

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -10,11 +10,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        if (constraints.maxWidth < 980) {
-          return Expanded(
-            child: Scaffold(
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 800) {
+            return Scaffold(
               drawer: Drawer(
                 width: constraints.maxWidth * .5,
                 child: GetBuilder<NavigationController>(
@@ -71,32 +71,118 @@ class HomeScreen extends StatelessWidget {
                   return controller.screens[currentIndex];
                 },
               ),
-            ),
-          );
-        }
-
-        return Scaffold(
-          body: Row(
-            children: [
-              const NavigationMenu(),
-              GetBuilder<NavigationController>(
-                builder: (controller) {
-                  int currentIndex = controller.currentIndex.value;
-                  return Expanded(
-                    child: controller.screens[currentIndex],
-                  );
-                },
+            );
+          } else {
+            return Scaffold(
+              body: Row(
+                children: [
+                  const NavigationMenu(),
+                  GetBuilder<NavigationController>(
+                    builder: (controller) {
+                      int currentIndex = controller.currentIndex.value;
+                      return Expanded(
+                        child: controller.screens[currentIndex],
+                      );
+                    },
+                  ),
+                ],
               ),
-            ],
-          ),
-          floatingActionButtonLocation: FloatingActionButtonLocation.miniStartFloat,
-          floatingActionButton: FloatingActionButton(
-            onPressed: () async => await FirebaseAuth.instance.signOut(),
-            child: const Icon(Icons.logout),
-            tooltip: "Logout",
-          ),
-        );
-      },
+              floatingActionButtonLocation: FloatingActionButtonLocation.miniStartFloat,
+              floatingActionButton: FloatingActionButton(
+                onPressed: () async => await FirebaseAuth.instance.signOut(),
+                child: const Icon(Icons.logout),
+                tooltip: "Logout",
+              ),
+            );
+          }
+        },
+      ),
     );
+    // return LayoutBuilder(
+    //   builder: (context, constraints) {
+    //     if (constraints.maxWidth < 800) {
+    //       return Scaffold(
+    //         drawer: Drawer(
+    //           width: constraints.maxWidth * .5,
+    //           child: GetBuilder<NavigationController>(
+    //             builder: (controller) {
+    //               int currentIndex = controller.currentIndex.value;
+    //               return ListView(
+    //                 padding: const EdgeInsets.only(top: 30),
+    //                 children: [
+    //                   NavigationButton(
+    //                     index: 0,
+    //                     currentIndex: currentIndex,
+    //                     title: "Products",
+    //                     icon: Icons.list,
+    //                     onTap: (index) {
+    //                       controller.updateIndex(index);
+    //                     },
+    //                   ),
+    //                   NavigationButton(
+    //                     index: 1,
+    //                     currentIndex: currentIndex,
+    //                     title: "Users",
+    //                     icon: Icons.person_rounded,
+    //                     onTap: (index) {
+    //                       controller.updateIndex(index);
+    //                     },
+    //                   ),
+    //                   NavigationButton(
+    //                     index: 2,
+    //                     currentIndex: currentIndex,
+    //                     title: "Orders",
+    //                     icon: Icons.shopping_cart,
+    //                     onTap: (index) {
+    //                       controller.updateIndex(index);
+    //                     },
+    //                   ),
+    //                   NavigationButton(
+    //                     index: 3,
+    //                     currentIndex: currentIndex,
+    //                     title: "Events",
+    //                     icon: Icons.shopping_cart,
+    //                     onTap: (index) {
+    //                       controller.updateIndex(index);
+    //                     },
+    //                   ),
+    //                 ],
+    //               );
+    //             },
+    //           ),
+    //         ),
+    //         appBar: AppBar(),
+    //         body: GetBuilder<NavigationController>(
+    //           builder: (controller) {
+    //             int currentIndex = controller.currentIndex.value;
+    //             return controller.screens[currentIndex];
+    //           },
+    //         ),
+    //       );
+    //     } else {
+    //       return Scaffold(
+    //         body: Row(
+    //           children: [
+    //             const NavigationMenu(),
+    //             GetBuilder<NavigationController>(
+    //               builder: (controller) {
+    //                 int currentIndex = controller.currentIndex.value;
+    //                 return Expanded(
+    //                   child: controller.screens[currentIndex],
+    //                 );
+    //               },
+    //             ),
+    //           ],
+    //         ),
+    //         floatingActionButtonLocation: FloatingActionButtonLocation.miniStartFloat,
+    //         floatingActionButton: FloatingActionButton(
+    //           onPressed: () async => await FirebaseAuth.instance.signOut(),
+    //           child: const Icon(Icons.logout),
+    //           tooltip: "Logout",
+    //         ),
+    //       );
+    //     }
+    //   },
+    // );
   }
 }
